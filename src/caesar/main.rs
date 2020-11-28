dc82098d7611883ff260a9c5c8ebaf388973faf7
White-Green <43771790+White-Green@users.noreply.github.com>
2020-11-29 03:08:51 +0900

    Caesar cipher

---
+++ src/caesar/main.rs
@@ -0,0 +1,66 @@
+use clap::{App, Arg};
+
+mod test;
+
+#[derive(Debug, PartialEq)]
+enum Error {
+    InputValueError(&'static str)
+}
+
+fn main() {
+    let matches = App::new("caesar")
+        .about("Caesar cipher")
+        .arg(Arg::new("decrypt")
+            .short('d')
+            .about("flag to decrypt"))
+        .arg(Arg::new("key")
+            .short('k')
+            .about("amount of rotate")
+            .takes_value(true)
+            .default_value("3"))
+        .arg(Arg::new("input")
+            .about("input value to encrypt or decrypt")
+            .required(true))
+        .get_matches();
+    if let Ok(key) = matches.value_of("key").unwrap().parse() {
+        if matches.is_present("decrypt") {
+            match decrypt(matches.value_of("input").unwrap(), key) {
+                Ok(result) => println!("{}", result),
+                Err(e) => eprintln!("error:{:?}", e),
+            }
+        } else {
+            match encrypt(matches.value_of("input").unwrap(), key) {
+                Ok(result) => println!("{}", result),
+                Err(e) => eprintln!("error:{:?}", e),
+            }
+        }
+    } else {
+        eprintln!("argument 'key' should be number");
+    }
+}
+
+const ALPHABET_COUNT: u8 = 26;
+
+fn encrypt(input: &str, key: usize) -> Result<String, Error> {
+    if input.chars().any(|c| !c.is_alphabetic() || !c.is_lowercase()) {
+        return Err(Error::InputValueError("All input characters should be lowercase alphabet."));
+    }
+    let mut result = String::with_capacity(input.chars().count());
+    let key = (key % ALPHABET_COUNT as usize) as u8;
+    for c in input.chars() {
+        result.push(((c as u8 - 'a' as u8 + key) % ALPHABET_COUNT + 'a' as u8) as char);
+    }
+    Ok(result)
+}
+
+fn decrypt(input: &str, key: usize) -> Result<String, Error> {
+    if input.chars().any(|c| !c.is_alphabetic() || !c.is_lowercase()) {
+        return Err(Error::InputValueError("All input characters should be lowercase alphabet."));
+    }
+    let mut result = String::with_capacity(input.chars().count());
+    let key = (key % ALPHABET_COUNT as usize) as u8;
+    for c in input.chars() {
+        result.push(((c as u8 - 'a' as u8 + ALPHABET_COUNT - key) % ALPHABET_COUNT + 'a' as u8) as char);
+    }
+    Ok(result)
+}
---
+++ src/caesar/test.rs
@@ -0,0 +1,23 @@
+use crate::{encrypt, decrypt};
+
+#[test]
+fn test_encrypt() {
+    assert_eq!(encrypt("abcdefghijklmnopqrstuvwxyz", 3), Ok("defghijklmnopqrstuvwxyzabc".to_string()));
+    assert_eq!(encrypt("abcdefghijklmnopqrstuvwxyz", 5), Ok("fghijklmnopqrstuvwxyzabcde".to_string()));
+    encrypt("A", 5).unwrap_err();
+    encrypt("0", 5).unwrap_err();
+    encrypt("+", 5).unwrap_err();
+    encrypt("/", 5).unwrap_err();
+    encrypt("!", 5).unwrap_err();
+}
+
+#[test]
+fn test_decrypt(){
+    assert_eq!(decrypt("defghijklmnopqrstuvwxyzabc", 3), Ok("abcdefghijklmnopqrstuvwxyz".to_string()));
+    assert_eq!(decrypt("fghijklmnopqrstuvwxyzabcde", 5), Ok("abcdefghijklmnopqrstuvwxyz".to_string()));
+    decrypt("A", 5).unwrap_err();
+    decrypt("0", 5).unwrap_err();
+    decrypt("+", 5).unwrap_err();
+    decrypt("/", 5).unwrap_err();
+    decrypt("!", 5).unwrap_err();
+}
